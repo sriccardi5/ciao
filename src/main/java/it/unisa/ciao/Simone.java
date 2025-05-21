8bd0c1b68d0c9aac5ebd314442dfd70f02016f1a
Andrea Sellitto <a.sellitto20@studenti.unisa.it>
2025-05-21 18:19:07 +0200

    prova ciao simone 3

---
+++ src/main/java/it/unisa/ciao/Simone.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.ciao;
+
+/**
+ *
+ * @author xandr
+ */
+public class Simone {
+    //ciao simone
+}
