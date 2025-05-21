8dc7da2875f85db52a791a36adf8c9004c97b04e
SR <simone.riccardi.18@gmail.com>
2025-05-21 18:08:03 +0200

    branch main

---
+++ src/main/java/it/unisa/ciao/Ciao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package it.unisa.ciao;
+
+/**
+ *
+ * @author mainuser
+ */
+public class Ciao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
