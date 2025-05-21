1335e87bcc7ccd78b0b82c08862e4473b5d43ddb
Andrea Sellitto <a.sellitto20@studenti.unisa.it>
2025-05-21 18:22:08 +0200

    modifica sysout

--- src/main/java/it/unisa/ciao/Ciao.java
+++ src/main/java/it/unisa/ciao/Ciao.java
@@ -13,6 +13,6 @@ package it.unisa.ciao;
 public class Ciao {
 
     public static void main(String[] args) {
-        System.out.println("ciao simone");
+        System.out.println("ciao simoneeee 56");
     }
 }
